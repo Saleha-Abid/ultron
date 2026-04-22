e3c2aad4dd3b52ac0ee8ea7dffed11a8562b6fb9
Saleha-Abid <159038010+Saleha-Abid@users.noreply.github.com>
2026-04-22 16:22:20 +0500

    commenting out some

--- keepalived/raven01/scripts/go_secondary.sh
+++ keepalived/raven01/scripts/go_secondary.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 /sbin/drbdadm up r0
 # 1. Stop NFS first to release file locks
-systemctl stop nfs-kernel-server
-systemctl stop lsyncd
+systemctl stop nfs-kernel-server #comment these out for now
+systemctl stop lsyncd #comment these out for now
 # 2. Unmount safely
 umount /data
 # 3. Demote DRBD
