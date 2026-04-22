b067459003bd7e277475534138a399ced491f786
Saleha-Abid <159038010+Saleha-Abid@users.noreply.github.com>
2026-04-22 16:22:53 +0500

    comments addinf

--- keepalived/raven02/scripts/go_primary.sh
+++ keepalived/raven02/scripts/go_primary.sh
@@ -5,6 +5,6 @@ drbdadm primary r0
 # 2. Mount the drive
 mount /dev/drbd0 /data
 # 3. Start NFS
-systemctl start nfs-kernel-server
-systemctl start lsyncd
+systemctl start nfs-kernel-server #comment these out now
+systemctl start lsyncd #comment these out for now
 
